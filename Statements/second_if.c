c7a669f57e9cb3f063850c613e4e8e9cea4cb02a
Lahquey <luckygriffin2@gmail.com>
2024-01-13 22:17:10 -0800

    if statement test2

---
+++ Statements/second_if.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int number;
+
+	printf("Bonyeza Nambari");
+	scanf("%d",&number);
+
+	printf("Nammbari uliyoibonyeza ni:%d\n",number);
+	if (number==7894){
+		printf("Ahoyyy!! Welcome to the game");
+	}
+	else {
+		printf("Wrong Pin; Is you really a Pirate?");
+	}
+
+
+	return 0;
+
+}
+
+
